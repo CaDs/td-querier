4328fa8ae5ade9fc79d853ceced1af2397a529b4
cads <cdonderis@gmail.com>
2013-05-22 17:47:41 +0900

    added rspec coverage, bugs fixed

--- lib/td/querier.rb
+++ lib/td/querier.rb
@@ -10,24 +10,24 @@ class Querier
     @api_key = api_key
   end
 
-  def query(database, query, opts, on_demand_path='', priority=1, reschedule_time=5.minutes)
+  def query(database, query, opts, on_demand_path='', priority=1, reschedule_time=300)
     client = TreasureData::Client.new(@api_key)
     job = client.query(database, query, on_demand_path, priority)
     Querier.perform_async(@api_key, job.job_id, opts, reschedule_time)
   end
 
-  def perform(api_key, job_id, opts, reschedule_time=5.minutes)
+  def perform(api_key, job_id, opts, reschedule_time=300)
     client = TreasureData::Client.new(api_key)
     job = client.job(job_id)
     #reschedule if the job is not finished
     return Querier.perform_in(reschedule_time, api_key, job_id, opts, reschedule_time) unless job.finished?
 
     if opts
-      klass = opts['klass']
-      meth = opts['method']
-      send_results = opts['results']
+      klass = opts[:klass]
+      meth = opts[:method]
+      send_results = opts[:results]
       results = (send_results.to_s == "true" ? job.results : nil)
-      class_eval(klass).send(meth.to_s, results)
+      eval(klass).send(meth.to_s, results)
     end
   end
 end
--- spec/querier_spec.rb
+++ spec/querier_spec.rb
@@ -2,34 +2,90 @@ require 'spec_helper'
 
 describe Querier do
   describe "Send the query to treasaure data" do
-    context "Object initializacion" do
-      it "will create a querier object given an api key string"
-    end
-
     context "Wrong parameters" do
-      it "will send a query to treasure data given that the key string and the rest of the params are correct"
 
-      it "will raise an error if the api key is not valid"
+      it "will raise an error if the api key is not valid" do
+        fake_client = Object.new
+        fake_client.should_receive(:query).and_raise(TreasureData::APIError.new("apikey authentication failed"))
+        TreasureData::Client.should_receive(:new).and_return(fake_client)
+        Querier.should_not_receive(:perform_async)
 
-      it "will raise an error if the database name is not valid"
+        querier = Querier.new("bad_key")
+        lambda{querier.query("database", "my_query", {})}.should raise_error(TreasureData::APIError)
+      end
 
-      it "will raise an error if the query is not valid"
+      it "will raise an error if the database name is not valid" do
+        fake_client = Object.new
+        fake_client.should_receive(:query).and_raise(TreasureData::NotFoundError.new("Couldn't find UserDatabase with name = bad_name"))
+        TreasureData::Client.should_receive(:new).and_return(fake_client)
+        Querier.should_not_receive(:perform_async)
+
+        querier = Querier.new("good_key")
+        lambda{querier.query("bad_name", "my_query", {})}.should raise_error(TreasureData::NotFoundError)
+      end
     end
 
     context "Right parameters" do
-      it "will send a query to treasure data and instance a sidekiq job with the job id of the new job"
+      it "will send a query to treasure data and instance a sidekiq job with the job id of the new job" do
+        fake_job = Object.new
+        fake_job.should_receive(:job_id).and_return(1)
+
+        fake_client = Object.new
+        fake_client.should_receive(:query).and_return(fake_job)
+
+        TreasureData::Client.should_receive(:new).and_return(fake_client)
+
+        Querier.should_receive(:perform_async).and_return(true)
+
+        querier = Querier.new("good_key")
+        querier.query("database", "my_query", {})
+      end
     end
   end
 
   describe "Sidekiq processing" do
     context "Standar processing" do
-      it "will reschedule the sidekiq job if the treasure data job is not yet finished"
+      before(:each) do
+        @opts = {:klass => "Object", :method => "fake_method", :results => "false"}
+        @fake_job = Object.new
+        @fake_client = Object.new
+        @fake_client.should_receive(:job).and_return(@fake_job)
+        TreasureData::Client.should_receive(:new).and_return(@fake_client)
+      end
 
-      it "will make a call to a given class method if que job is finished"
+      it "will reschedule the sidekiq job if the treasure data job is not yet finished" do
+        @fake_job.should_receive(:finished?).and_return(false)
+        Querier.should_receive(:perform_in).with(300, "apikey", 1, @opts, 300).and_return(true)
+        Querier.new.perform("apikey", 1, @opts)
+      end
+
+      it "will make a call to a given class method if que job is finished" do
+        @fake_job.should_receive(:finished?).and_return(true)
+        Object.should_receive(:fake_method).and_return(true)
+        Querier.new.perform("apikey", 1, @opts)
+      end
+
+      it "will request the query results if the options for result are set to true" do
+        new_opts = {:klass => "Object", :method => "fake_method", :results => "true"}
+        results = [[1]]
+        @fake_job.should_receive(:finished?).and_return(true)
+        @fake_job.should_receive(:results).and_return(results)
+        Object.should_receive(:fake_method).and_return(true)
+        Querier.new.perform("apikey", 1, new_opts)
+      end
     end
 
     context "Error case" do
-      it "will raise an exception if something explodes within sidekiq"
+      it "will raise an exception if something explodes within sidekiq" do
+        @opts = {:klass => "Object", :method => "fake_method", :results => "false"}
+        @fake_job = Object.new
+        @fake_client = Object.new
+        @fake_client.should_receive(:job).and_return(@fake_job)
+        TreasureData::Client.should_receive(:new).and_return(@fake_client)
+
+        @fake_job.should_receive(:finished?).and_raise(RuntimeError.new("unexpected error"))
+        lambda{Querier.new.perform("apikey", 1, @opts)}.should raise_error(RuntimeError)
+      end
     end
   end
 end
